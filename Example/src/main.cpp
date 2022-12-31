4f29082f1678add632b8cfda11bdf703d93c24ea
LoRd_MuldeR <mulder2@gmx.de>
2022-12-31 14:00:15 +0100

    Added example application.

---
+++ Example/src/main.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************/
+/* CompatLib for Windows XP, by LoRd_MuldeR <MuldeR2@GMX.de>                  */
+/* This work has been released under the CC0 1.0 Universal license!           */
+/******************************************************************************/
+
+#include <iostream>
+
+#ifdef _M_X64
+#  define ARCH_TYPE "x64"
+#else
+#  define ARCH_TYPE "x86"
+#endif
+
+int main()
+{
+    std::cout << "Hello World! [" << ARCH_TYPE << "]" << std::endl;
+}
