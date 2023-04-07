a6f499ce5029c321554af79c946ec8698f71ff19
LoRd_MuldeR <mulder2@gmx.de>
2023-04-07 22:34:47 +0200

    Use the "__acrt_*" downlevel functions.

--- CompatLib/src/compat_x86.asm
+++ CompatLib/src/compat_x86.asm
@@ -7,19 +7,19 @@
 
 .data
 
-__imp__CompareStringEx@36 dd _XPCompatLib_CompareStringEx@36
-__imp__GetLocaleInfoEx@16 dd _XPCompatLib_GetLocaleInfoEx@16
-__imp__InitializeCriticalSectionEx@12 dd _XPCompatLib_InitializeCriticalSectionEx@12
-__imp__LCMapStringEx@36 dd _XPCompatLib_LCMapStringEx@36
+__imp__CompareStringEx@36 dd ___acrt_CompareStringEx@36
+__imp__GetLocaleInfoEx@16 dd ___acrt_GetLocaleInfoEx@16
+__imp__InitializeCriticalSectionEx@12 dd ___acrt_InitializeCriticalSectionEx@12
+__imp__LCMapStringEx@36 dd ___acrt_LCMapStringEx@36
 
 externdef __imp__CompareStringEx@36 : DWORD
 externdef __imp__GetLocaleInfoEx@16 : DWORD
 externdef __imp__InitializeCriticalSectionEx@12 : DWORD
 externdef __imp__LCMapStringEx@36 : DWORD
 
-extern _XPCompatLib_CompareStringEx@36 : proc
-extern _XPCompatLib_GetLocaleInfoEx@16 : proc
-extern _XPCompatLib_InitializeCriticalSectionEx@12 : proc
-extern _XPCompatLib_LCMapStringEx@36 : proc
+extern ___acrt_CompareStringEx@36 : proc
+extern ___acrt_GetLocaleInfoEx@16 : proc
+extern ___acrt_InitializeCriticalSectionEx@12 : proc
+extern ___acrt_LCMapStringEx@36 : proc
 
 end
